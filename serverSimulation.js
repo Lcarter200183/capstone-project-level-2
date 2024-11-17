a6477d7bec2f37369c2555f80410a208657aa402
Lydia Carter <lcarter200183@yahoo.comgit config --global lcarter200183@yahoo.com>
2024-11-17 15:55:09 -0600

    updated files

--- serverSimulation.js
+++ serverSimulation.js
@@ -7,6 +7,27 @@
         document.write(message);
     }
 
+    function greeting() {
+        display("Hello World!");
+    }
+
+    function serverResponse() {
+        display("Form was successfully processed.");
+    }
+
+    function myFunction(callbackFunction) {
+        callbackFunction();
+    }
+
+    function activator() {
+        myFunction(greeting);
+    }
+
+    function delayedGreeting() {
+        setTimeout(greeting, 5000);
+    }
+
+
     function asychronous() {
         setTimeout(greeting, 5000);
         display("Waiting for the greeting...");
@@ -22,7 +43,11 @@
         setTimeout(serverResponse, 5000);
     }
 
-    function serverResponse() {
+    function myFunction(callbackFunction) {
+        callbackFunction();
+    }
+
+    function serverResponse(resolve) {
         display("Successfully processed.");
         resolve("The promise is fulfilled.");
     }
@@ -31,7 +56,18 @@
         debugger;
         setTimeout(serverResponse2, 5000);
         display("Inside my myFunction");
+
+
+        function serverResponse2(resolve) {
+            display("Form was successfully processed.");
+            resolve("The promise is fulfilled");
+        }
     }
+
+    function activator(myParameter) {
+        display("myParameter");
+    }
+
 
     function displayObject() {
         const myCar = {
@@ -49,33 +85,70 @@
     }
 
     function myFunction(resolve) {
+
         display("Inside my myFunction");
         resolve();
     }
 
     function makePromise() {
+        debugger;
         const promise = new Promise(myFunction);
         promise.then(getMessage);
-        display("Made a promise");
+        display("Made a promise.");
+    }
+
+    function makeRequest() {
+        debugger;
+        const promise = new Promise(getServerResponse);
+        return promise;
     }
 
     function getMessage(resolveValue) {
+        debogger;
         display(resolveValue);
     }
 
+    function getServerResponse(resolve) {
+        setTimeout(activateResolve, 5000);
+
+        function activateResolve(resolve) {
+
+            const response = {
+                message: "Successfully processed.",
+            };
+            const resolveValue = JSON.stringify(response);
+            resolve(resolveValue);
+        }
+    }
+
     function parseResponse(resolveValue) {
+        //THE RESOLVE VALUE WILL BE A FETCH OBJECT
+        //THE TEXT METHOD EXTRACTS THE STRINGIFIED OBJECT
         const response = JSON.parse(resolveValue);
         const message = response.message;
         display(message);
     }
 
+    function parseResponse(resolveValue) {
+        const promise = resolveValue.test();
+        promise.then(viewResponse);
+    }
+
+    function outerFunction() {
+        const message = "Message from the outerFunction";
+        innerFunction();
+
+        function innerFunction() {
+            display(message);
+        }
+    }
 
     function handleSubmit(event) {
         event.preventDefault();
         const inputs = event.target;
-        const emailInput = Input[0];
+        const emailInput = inputs[0];
         const email = emailInput.value;
         display("Submitting form for" + email + "...");
-        const promise = makeRequest("https://myserver.com")
-        promise.then(parseResponse)
+        const promise = makeRequest("getServerResponse");
+        promise.then(parseResponse);
     }
