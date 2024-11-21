9d92f0c83bcff7a1287e11177219e46091c4a206
Lydia Carter <lcarter200183@yahoo.comgit config --global lcarter200183@yahoo.com>
2024-11-21 13:14:04 -0600

    fix file

--- form.js
+++ form.js
@@ -1,19 +1,42 @@
-function myProcessForm(event) {
-    debugger
+function myprocessForm(event) {
     event.preventDefault();
     const form = event.target;
     const input = form[0];
     const value = input.value;
-    output("Your appointment was submitted.");
+    output("Your appointment was submitted successfully.");
 }
 
 
 
 function emailForm(event) {
-    debugger;
     event.preventDefault();
     const emailForm = event.target;
     const emailInput = emailForm[0];
     const emailvalue = emailInput.value;
-    output("Your appointment was submitted.");
+    output("Your appointment was submitted successfully.");
+}
+
+function nameForm(event) {
+    event.preventDefault();
+    const form = event.target;
+    const firstnameInput = form[0];
+    const lastnameInput = form[1];
+    const firstnamevalue = firstnameInput.value;
+    output("Your appointment was submitted successfully.");
+}
+
+function numberForm(event) {
+    event.preventDefault();
+    const numberform = event.target;
+    const numberInput = numberform[0];
+    const numberValue = numberInput.value;
+    output("Your appointment was submitted successfully.");
+}
+
+function datetimeForm(event) {
+    event.preventDefault();
+    const datetimeform = event.target;
+    const datetimeInput = datetimeform[0];
+    const datetimevalue = datetimeInput.value;
+    output("Your appointment was submitted successfully.");
 }
