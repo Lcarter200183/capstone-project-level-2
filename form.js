ad560b548fd3fe441635848ee0d61f6cbeb7b926
Lydia Carter <lcarter200183@yahoo.comgit config --global lcarter200183@yahoo.com>
2024-11-24 21:22:52 -0600

    fixed file

--- form.js
+++ form.js
@@ -1,8 +1,18 @@
+function myprocessForm(event) {
+    debugger;
+    event.preventDefault();
+    const form = event.target;
+    const input = form[0];
+    const Value = input.value;
+    output("Your info was submitted.");
+}
+
+
 function emailForm(event) {
     debugger;
     event.preventDefault();
     const emailForm = event.target;
     const emailInput = emailForm[0];
     const emailvalue = emailInput.value;
-    output("Your appointment was submitted.");
+    output("Your message was submitted.");
 }
